071cd26639508fc81ce72592f795dc1eea2b88a8
tom-sb <fvillanuevanut@gmail.com>
2016-09-20 12:38:03 -0500

    Create cuentacaractereshastaemter.c

---
+++ cuentacaractereshastaemter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int cont;
+    
+    /*while(getchar()!='\n') ++cont;
+    printf("%d\n",cont);*/
+    
+    for(cont=1;getchar()!='\n';++cont)
+    
+    printf("%i\n",cont);
+
+}
